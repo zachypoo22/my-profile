41433fa8bd14008fe81a381737a4bf41777b50d3
Zacheriah Mell <zachm93@live.com>
2018-09-12 20:37:38 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,13 +18,13 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.XenVM("node12")
+node = request.XenVM("node14")
 
 # use centos
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 
 #get a routable IP
-node.routable_control_ip = "true"
+# node.routable_control_ip = "true"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
