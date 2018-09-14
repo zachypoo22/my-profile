7b743a7f4d4148cb888a58420f312ee2fef3d52c
Zacheriah Mell <zachm93@live.com>
2018-09-14 11:39:19 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,33 +5,6 @@ This particular profile is a simple example of using a single raw PC. It can be 
 Instructions:
 Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
 """
-"""
-# Import the Portal object.
-import geni.portal as portal
-# Import the ProtoGENI library.
-import geni.rspec.pg as pg
-
-# Create a portal context.
-pc = portal.Context()
-
-# Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.XenVM("node0")
-
-# use centos
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-
-#get a routable IP
-node.routable_control_ip = "true"
-
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
-"""
 
 # ---- try two
 import geni.portal as portal
@@ -42,7 +15,7 @@ request = portal.context.makeRequestRSpec()
 # Create a XenVM
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routable_control_ip = True
+node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
                               command="/local/repository/silly.sh"))
